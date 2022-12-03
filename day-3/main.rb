dfd31c20b2bd4312689408e2feb0481f87868e1a
Josh Menden <josh.menden@hey.com>
2022-12-02 22:24:50 -0700

    Part 2 complete

--- day-3/main.rb
+++ day-3/main.rb
@@ -1,35 +1,19 @@
 #!/usr/bin/env ruby
 
 require "byebug"
+require "active_support/all"
 fname = "input.txt"
 content = File.read(fname)
 
-class Rucksack
-    attr_accessor :compartments
-    def initialize(rucksack_str)
-      rucksack_arr = rucksack_str.split("")
-      @compartments = rucksack_arr.each_slice((rucksack_arr.count/2.0).round).to_a
+total = 0
+content.split("\n").in_groups_of(3).each do |r_group|
+    commons = r_group[0].split("") & r_group[1].split("") & r_group[2].split("")
+    c = commons[0]
+    if c == c.upcase
+        total += (c.ord - 38)
+    else
+        total += (c.ord - 96)
     end
-
-    def commons
-        @compartments[0] & @compartments[1]
-    end
-
-    def priority_value
-        c = commons[0]
-        if c == c.upcase
-            return c.ord - 38
-        else
-            return c.ord - 96
-        end
-    end
-    
 end
 
-rucksacks = []
-content.split("\n").each do |rucksack_str|
-    r = Rucksack.new(rucksack_str)
-    rucksacks << r
-end
-
-puts rucksacks.map(&:priority_value).inject(0, :+)
+puts total
