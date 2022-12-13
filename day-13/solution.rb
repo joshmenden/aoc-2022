e3fb5442173a099ad6639bae541fd2100e59a657
Josh Menden <josh.menden@hey.com>
2022-12-12 23:39:58 -0700

    day 13 pt 1 complete

--- day-13/solution.rb
+++ day-13/solution.rb
@@ -6,7 +6,6 @@ require "active_support/all"
 valid_pairs = []
 
 def compare_packets(p1, p2)
-    # byebug
     v1 = p1.shift
     v2 = p2.shift
 
@@ -15,32 +14,38 @@ def compare_packets(p1, p2)
     elsif !v1.nil? && v2.nil?
         return false
     elsif v1.nil? && v2.nil?
-        return true
-    elsif v1.is_a?(Array) && v2.is_a?(Array)
-        return compare_packets(v1, v2) && compare_packets(p1, p2)
+        return nil
+    end
+
+    first_comp = nil
+    if v1.is_a?(Array) && v2.is_a?(Array)
+        first_comp = compare_packets(v1, v2)
     elsif v1.is_a?(Array) && !v2.is_a?(Array)
-        return compare_packets(v1, [v2]) && compare_packets(p1, p2)
+        first_comp = compare_packets(v1, [v2])
     elsif !v1.is_a?(Array) && v2.is_a?(Array)
-        return compare_packets([v1], v2) && compare_packets(p1, p2)
+        first_comp = compare_packets([v1], v2)
     elsif !v1.is_a?(Array) && !v2.is_a?(Array)
         if v1 == v2
-            return compare_packets(p1, p2)
+            first_comp = nil
         else
-            return v1 < v2
-            # return v1 < v2 && compare_packets(p1, p2) # problem? need to compare all values? need an in between val?
+            first_comp = v1 < v2
         end
     end
+
+    return compare_packets(p1, p2) if first_comp.nil?
+    return false if first_comp == false
+    return true if first_comp == true
 end
 
+# File.read("i.txt").split("\n\n").each_with_index do |pair, index|
 File.read("input.txt").split("\n\n").each_with_index do |pair, index|
-    # byebug
     p1, p2 = pair.split("\n").map {|c| JSON.parse(c) }
     if compare_packets(p1, p2)
         valid_pairs << index + 1
     end
 end
 
-byebug
+# byebug
 puts valid_pairs.reduce(:+)
 
 
